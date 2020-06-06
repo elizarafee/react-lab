5e8c4582702651285a9afc1a49a8441c642ff52f
Eliza Ahmed <elizarafee@gmail.com>
2020-06-06 21:54:46 +0600

    class component : destructuring state

--- src/components/propsClass.component.js
+++ src/components/propsClass.component.js
@@ -1,11 +1,23 @@
 import React, {Component} from 'react'
 
 class Prop extends Component {
+    constructor(){
+        super();
+
+        this.state = {
+            state1 : 'Hasan',
+            state2 : 'Tareque'
+        }
+    }
+    
     render(){
     const {fnamee, snamee} = this.props
+    const {state1, state2} = this.state
+    
     return (
     <div>
-    <h3>first name : {fnamee}, last name : {snamee}</h3>
+    <h3>first props : {fnamee}, last props : {snamee}</h3>
+    <h3>first state : {state1}, last state : {state2}</h3>
     <p>{this.props.children}</p>
     </div>
     )
