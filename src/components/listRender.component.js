ca0f98eddc6f8664b2fdf2b1695a8d9de5c79cf7
Eliza Ahmed <elizarafee@gmail.com>
2020-06-09 00:33:19 +0600

    list rendering : 4th approach

--- src/components/listRender.component.js
+++ src/components/listRender.component.js
@@ -1,11 +1,26 @@
 import React from 'react'
 
 const ListRender = () => {
-
-    const Arr = ['hello', 'world!', 'you', 'are', 'fake']
-    const value = Arr.map(x => <h2>{x}</h2>)
+    const Arr = [
+        {
+            id : 1,
+            name : 'Hasan',
+            school : 'AAAAA'
+        },
+        {
+            id : 2,
+            name : 'Tareque',
+            school : 'BBBBB'
+        },
+        {
+            id : 3,
+            name : 'Rafee',
+            school : 'CCCCCC'
+        }
+    ]
+    const value = Arr.map(x => <h3>name : {x.name}, school : {x.school}</h3>)
     return (
-        // List rendering : 3rd approach
+        // List rendering : 4th approach
         <div>{value}</div>
     )
 }
