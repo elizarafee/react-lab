22741e589f5d0ac56da0220d2806346b2967270a
Eliza Ahmed <elizarafee@gmail.com>
2020-06-09 16:23:45 +0600

    list rendering : index as key

--- src/components/listRender.component.js
+++ src/components/listRender.component.js
@@ -2,25 +2,8 @@ import React from 'react'
 import Value from './value.component'
 
 const ListRender = () => {
-    const Arr = [
-        {
-            id : 1,
-            name : 'Hasan',
-            school : 'AAAAA'
-        },
-        {
-            id : 2,
-            name : 'Tareque',
-            school : 'BBBBB'
-        },
-        {
-            id : 3,
-            name : 'Rafee',
-            school : 'CCCCCC'
-        }
-    ]
-    // in list rendering key props have to be included and it need to have a unique id. and the  key props is not accessibe from the child component
-    const value = Arr.map(x => <Value key={x.id} val={x} />)
+    const Arr = ['Hasan', 'Tareque', 'Eliza', 'Rafee'];
+    const value = Arr.map((val, index) => <h2 key={index}>{index} {val}</h2>)
     return (
         // List rendering : 4th approach
         <div>{value}</div>
