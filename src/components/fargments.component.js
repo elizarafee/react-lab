b9ee025319babed36cfc26cd3b317240dd914854
Eliza Ahmed <elizarafee@gmail.com>
2020-06-20 00:27:12 +0600

    react fragments : key attribute

--- src/components/fargments.component.js
+++ src/components/fargments.component.js
@@ -5,11 +5,19 @@
 import React from 'react'
 
 const Fargments = () => {
+    const items = ['abc', 'bcd', 'cde', 'def', 'efg']
     return (
-        <>
+        <React.Fragment>
+            {
+            items.map((val, index) => (
+                <React.Fragment key={index}>
+                    <h1>{index} {val}</h1>
+                </React.Fragment>
+            ))
+            }
             <h1>The Cow</h1>
             <p>Cow is a domestic animal</p>
-        </>
+        </React.Fragment>
     )
 }
 
